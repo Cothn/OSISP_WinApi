e0e7355602dc4b9f1219e718419c015c80d8ed28
Cothn <Sevkaa-0013@mail.ru>
2019-09-29 19:46:25 +0400

    Start laba2

---
+++ Laba2(table)/WindowsProject1/WindowsProject1/framework.h
@@ -0,0 +1,12 @@
+﻿// header.h: включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта
+//
+
+#pragma once
+
+#include <SDKDDKVer.h>
+// Файлы заголовков среды выполнения C
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
