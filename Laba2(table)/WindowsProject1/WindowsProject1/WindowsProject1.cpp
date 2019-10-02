d911fd9167974ba9d93121fcbb715b9c35469d33
Cothn <Sevkaa-0013@mail.ru>
2019-10-02 11:16:07 +0400

    paint line

--- Laba2(table)/WindowsProject1/WindowsProject1/WindowsProject1.cpp
+++ Laba2(table)/WindowsProject1/WindowsProject1/WindowsProject1.cpp
@@ -3,7 +3,7 @@
 
 #include <windows.h>
 //#include <wingdi.h>
-#include "Resource.h";
+#include "Resource.h"
 
 
 
@@ -11,6 +11,10 @@
 HINSTANCE hInst;                                // текущий экземпляр
 WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
 WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+#define Cwidth 3
+#define Cheigth 8
+
+void DrawTable(HDC hdc, int n, int m, RECT field);
 
 // Отправить объявления функций, включенных в этот модуль кода:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -101,22 +105,57 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // код прорисовки, использующий HDC...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+	HBRUSH hBrush; //создаём объект-кисть
+	PAINTSTRUCT ps;
+	HDC hdc; //создаём контекст устройства
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+	switch (message)
+	{
+
+	case WM_LBUTTONUP:
+	{
+		//if (is_player_catch)
+		UpdateWindow(hWnd);
+	}
+	break;
+	case WM_SIZE:
+	{
+		//OnWindowResize(&player, rect);
+		UpdateWindow(hWnd);
+	}
+	break;
+	case WM_PAINT:
+	{
+		hdc = BeginPaint(hWnd, &ps);
+		// код прорисовки, использующий HDC...
+		DrawTable(hdc, Cwidth, Cheigth, rect);
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
+	void DrawTable(HDC hdc, int n, int m, RECT field)
+	{
+		LONG width = (field.right - field.left) / m;
+		LONG height = (field.bottom - field.top) / n;
+		int k;
+		HBRUSH hBrush;
+		hBrush = CreateSolidBrush(RGB(250,200,100));//создание кисти
+		SelectObject(hdc, hBrush);//выбор кисти
+		for (k = 1; k < n; k++) {
+			MoveToEx(hdc, field.left, k*height, NULL); //сделать текущими координаты x1, y1
+			LineTo(hdc, field.right, k* height);
+		}
+		for (k = 1; k < m; k++) {
+			MoveToEx(hdc, k * width, field.top, NULL); //сделать текущими координаты x1, y1
+			LineTo(hdc, k * width, field.bottom);
+		}
+	}
 
